6ad14dacec55aca1f463fb66d62b18af1b3237ac
Gordan <gordan.pendic5@gmail.com>
2019-01-29 09:28:00 +0100

    Mongoose setup
    - mongoose added
    - Author added
    - Book added
    - addAuthor mutation added
    - addBook mutation added
    - mutation added to the exports -> now: queries and mutations

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to the database
+mongoose.connect('mongodb://root:2431996fkbask@ds225382.mlab.com:25382/grapql-tut', { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+    console.log('Connected to the database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+const Book = require('../models/book');
+const Author = require('../models/author');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 
@@ -45,10 +47,10 @@ var books = [
 ];
 
 var authors = [
-    {name: 'John', age: 44, id: '1', bookId: '1'},
-    {name: 'Mike', age: 33, id: '2', bookId: '2'},
-    {name: 'Tom', age: 66, id: '3', bookId: '3'},
-    {name: 'J. R. R. Tolkien', age: 66, id: '4', bookId: '4'}
+    {name: 'John', age: 44, id: '1'},
+    {name: 'Mike', age: 33, id: '2'},
+    {name: 'Tom', age: 66, id: '3'},
+    {name: 'J. R. R. Tolkien', age: 66, id: '4'}
 ];
 
 const RootQuery = new GraphQLObjectType({
@@ -101,6 +103,43 @@ const RootQuery = new GraphQLObjectType({
     }
 });
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: {type: GraphQLString},
+                age: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                });
+                return author.save();
+            }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: {type: GraphQLString},
+                genre: { type: GraphQLString },
+                authorId: { type: GraphQLID }
+            },
+            resolve(parent, args) {
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                });
+                return book.save();
+            }
+        }
+    }
+});
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
