a3f88be8dffcd11a802d2ae6d14f2b914f0f2e36
Mihajlo Nikodijevic <a01646292@unet.univie.ac.at>
2019-01-29 02:57:27 +0100

    GraphQLID and graphiql added.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,44 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
+});
+
+var books = [
+    {name: 'Book1 name', genre: 'Fantasy', id: '1'},
+    {name: '300', genre: 'Action', id: '2'},
+    {name: '6th Sense', genre: 'Sci-Fi', id: '3'}
+];
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        /**
+         * Getting a Book with specific id
+         * frontend calling: book(id: 'id_string') { fields to get }
+         * @param {string} id - id of the book
+         * @returns {Book}
+         * */
+        book: {
+            type: BookType,
+            args: {id: { type: GraphQLID }},
+            resolve(parent, args) {
+                // code to get data from db / other source
+                console.log('type: ' + typeof(args.id) + ' id: ' + args.id);
+                return _.find(books, {id: args.id});
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
