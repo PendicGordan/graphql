d84c6c20a2e549dd5b055e26d29b63e863e2d3d5
Gordan <gordan.pendic5@gmail.com>
2019-01-29 03:13:26 +0100

    .idea deleted.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -12,10 +12,25 @@ const BookType = new GraphQLObjectType({
     })
 });
 
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    })
+});
+
 var books = [
     {name: 'Book1 name', genre: 'Fantasy', id: '1'},
     {name: '300', genre: 'Action', id: '2'},
     {name: '6th Sense', genre: 'Sci-Fi', id: '3'}
+];
+
+var authors = [
+    {name: 'John', age: 44, id: '1'},
+    {name: 'Mike', age: 33, id: '2'},
+    {name: 'Tom', age: 66, id: '3'}
 ];
 
 const RootQuery = new GraphQLObjectType({
@@ -34,6 +49,21 @@ const RootQuery = new GraphQLObjectType({
                 // code to get data from db / other source
                 console.log('type: ' + typeof(args.id) + ' id: ' + args.id);
                 return _.find(books, {id: args.id});
+            }
+        },
+        /**
+         * Getting an Author with specific id
+         * frontend calling: author(id: 'id_string') { fields to get }
+         * @param {string} id - id of the author
+         * @returns {Author}
+         * */
+        author: {
+            type: AuthorType,
+            args: {id: { type: GraphQLID }},
+            resolve(parent, args) {
+                // code to get data from db / other source
+                console.log('type: ' + typeof(args.id) + ' id: ' + args.id);
+                return _.find(authors, {id: args.id});
             }
         }
     }
