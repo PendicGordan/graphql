2074dcb7b82635a17ffa7f4f27f8fe44a8528d1e
Gordan <gordan.pendic5@gmail.com>
2019-02-15 08:39:17 +0100

    .env added

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config({ path: ".env" });
 
 const app = express();
 
@@ -10,16 +11,19 @@ const app = express();
 app.use(cors());
 
 // connect to the database
-mongoose.connect('mongodb://root:2431996fkbask@ds225382.mlab.com:25382/grapql-tut', { useNewUrlParser: true });
-mongoose.connection.once('open', () => {
-    console.log('Connected to the database');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+mongoose.connection.once("open", () => {
+  console.log("Connected to the database");
 });
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
     graphiql: true
-}));
+  })
+);
 
 app.listen(4000, () => {
-    console.log('Server is running on port 4000');
+  console.log("Server is running on port 4000");
 });
